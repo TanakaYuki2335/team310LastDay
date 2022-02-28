090539ba5ba0272f927b7f3035630758c86385f8
Rinkodaisuki <100543774+Rinkodaisuki@users.noreply.github.com>
2022-02-28 12:12:07 +0900

    カメラ追従スクリプト
    MainCameraにアタッチして投影を「平行投影」からサイズの数字を大きくするとカメラが引いてくれます。
    X軸のみカメラ追従します。

---
+++ CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    //プレイヤーを格納する変数
+    public GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 PlayerPos = Player.transform.position;
+
+        //カメラとプレイヤーの位置を同じにする
+        transform.position = new Vector3(PlayerPos.x, 0, -10);
+    }
+}
